a950de1f02e119c7658be0e9890f2f686e296b13
Varshini895 <varshinireddynallamilli@gmail.com>
2024-08-09 23:13:43 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -8,7 +8,7 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    return (celcius<200?200:500);
 }
 
 void alertInCelcius(float farenheit) {
@@ -19,7 +19,7 @@ void alertInCelcius(float farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
